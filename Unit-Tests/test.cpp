907b1f1cec3abd8e50aa7f1c05fb743697bbca81
Boaz <boaz23mw@gmail.com>
2023-01-13 19:33:50 +0200

    Tests: Fixed execution order advanced test

--- Unit-Tests/test.cpp
+++ Unit-Tests/test.cpp
@@ -11,9 +11,16 @@ protected:
     {}
 };
 
-TEST_F(SemaphoreBinaryBasicTest, BasicUpDown) {
+TEST_F(SemaphoreBinaryBasicTest, BasicUpDown1) {
     s.down();
     s.up();
+}
+
+TEST_F(SemaphoreBinaryBasicTest, BasicUpDown2)
+{
+    s.down();
+    s.up();
+    s.down();
 }
 
 TEST(SemaphoreBinaryTests, ExecutionOrder1)
@@ -22,22 +29,16 @@ TEST(SemaphoreBinaryTests, ExecutionOrder1)
     {
         Semaphore_Binary s{ false };
         int n{ 5 }, no{ n }, nm{ n + 1 };
-        std::thread t1
+        std::thread t1{[&s, &n, &nm]()
         {
-            [&s, &n, &nm]
-            {
-                nm = n;
-                s.up();
-            }
-        };
-        std::thread t2
+            nm = n;
+            s.up();
+        }};
+        std::thread t2{[&s, &n]()
         {
-            [&s, &n]
-            {
-                s.down();
-                n--;
-            }
-        };
+            s.down();
+            n--;
+        }};
 
         t1.join();
         t2.join();
@@ -46,49 +47,52 @@ TEST(SemaphoreBinaryTests, ExecutionOrder1)
     }
 }
 
-//TEST(SemaphoreBinaryTests, ExecutionOrder2)
-//{
-//    Semaphore_Binary s1{true}, s2{false};
-//    int n{ 5 }, result{ 0 };
-//    std::thread t_adv
-//    {
-//        [&s1, &s2, &n]
-//        {
-//            while (n > 0)
-//            {
-//                s1.down();
-//                n--;
-//                s2.up();
-//            }
-//            s1.down();
-//        }
-//    };
-//    std::thread t_adder
-//    {
-//        [&s1, &s2, &n, &result]
-//        {
-//            if (n == 0)
-//            {
-//                s1.up();
-//                return;
-//            }
-//
-//            while (n >= 0)
-//            {
-//                s2.down();
-//                result += 2*n + 1;
-//                s1.up();
-//            }
-//        }
-//    };
-//
-//    t_adv.join();
-//    t_adder.join();
-//    EXPECT_EQ(n*n, result);
-//}
+TEST(SemaphoreBinaryTests, ExecutionOrder2)
+{
+    auto f = [](int n)
+    {
+        Semaphore_Binary s1{ true }, s2{ false };
+        int result{ 0 }, no{ n };
+        std::thread t_adv{ [&s1, &s2, &n]
+        {
+            while (n >= 0)
+            {
+                s1.down();
+                n--;
+                s2.up();
+            }
+            s1.down();
+        } };
+        std::thread t_adder{ [&s1, &s2, &n, &result]
+        {
+            while (true)
+            {
+                s2.down();
+                if (n < 0)
+                {
+                    s1.up();
+                    break;
+                }
+                result += 2*n + 1;
+                s1.up();
+            }
+        } };
+
+        t_adder.join();
+        t_adv.join();
+        EXPECT_EQ(no*no, result);
+    };
+
+    f(0);
+    f(1);
+    f(5);
+}
 
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    int exitCode = RUN_ALL_TESTS();
+    //std::cout << "Press any key to exit . . . ";
+    //std::cin.ignore();
+    return exitCode;
 }
